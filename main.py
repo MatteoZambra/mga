758b51d22c3ee65faf6700a2229566ed21842f48
MatteoZambra <matteo.zambra1@gmail.com>
2025-09-11 08:24:30 +0200

    Dismiss simulation

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ if __name__ == "__main__":
         )
     
     # Instantiate class to extrapolate spending patterns
-    if True:
+    if False:
         from copy import deepcopy
         import os
         
